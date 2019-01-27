ea3d55c0969c674e6a47779479856e710412d241
krzysztofbogusz91 <krzysztofbogusz050591@gmail.com>
2019-01-27 19:24:55 +0100

    ref2

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-   const list = document.querySelector('.list');
-   const strongCounter = (event) => {
-    // let arr = [];
- // for(l = 1; l <=6; l++){
- //     arr.push(l);
- // }
- // event.target.innerText = arr.reduce((a,b)=>(a*b))'
-
- // albo lepsze:
- let num = 1;
- for(l = 1; l <=6; l++){
-     num = l * num;
- }
- event.target.innerText = num;
-}
-   
-   for(let i = 1; i <= 12; i++){
-       const listElem = document.createElement('li');    
-       listElem.innerText = i;
-       listElem.className = `list-elem-${i}`;
-       if(i === 6){
-           listElem.addEventListener('click', strongCounter);
-        }
-        list.appendChild(listElem)
+  const list = document.querySelector('.list');
+  const strongCounter = event => {
+    let num = 1;
+    for (l = 1; l <= 6; l++) {
+      num = l * num;
     }
-        const lastElem = document.querySelector('.list-elem-12');
-        lastElem.addEventListener('click', () => {
-                lastElem.classList.add('animation');
-            })
-    })
+    event.target.innerText = num;
+  };
+
+  for (let i = 1; i <= 12; i++) {
+    const listElem = document.createElement('li');
+    listElem.innerText = i;
+    listElem.className = `list-elem-${i}`;
+    if (i === 6) {
+      listElem.addEventListener('click', strongCounter);
+    }
+    list.appendChild(listElem);
+  }
+  const lastElem = document.querySelector('.list-elem-12');
+  lastElem.addEventListener('click', (e) => {
+    e.target.classList.add('animation');
+  });
+});
