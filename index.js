bbb45a331bec0de380f600674f9de342975290c7
krzysztofbogusz91 <krzysztofbogusz050591@gmail.com>
2019-01-27 19:23:09 +0100

    refactor

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
    const list = document.querySelector('.list');
-   
+   const strongCounter = (event) => {
+    // let arr = [];
+ // for(l = 1; l <=6; l++){
+ //     arr.push(l);
+ // }
+ // event.target.innerText = arr.reduce((a,b)=>(a*b))'
+
+ // albo lepsze:
+ let num = 1;
+ for(l = 1; l <=6; l++){
+     num = l * num;
+ }
+ event.target.innerText = num;
+}
    
    for(let i = 1; i <= 12; i++){
        const listElem = document.createElement('li');    
        listElem.innerText = i;
        listElem.className = `list-elem-${i}`;
        if(i === 6){
-           listElem.addEventListener('click', () => {
-            // let arr = [];
-         // for(l = 1; l <=6; l++){
-         //     arr.push(l);
-         // }
-         // listElem.innerText = arr.reduce((a,b)=>(a*b))'
-
-         // albo lepsze:
-         let num = 1;
-         for(l = 1; l <=6; l++){
-             num = l * num;
-         }
-         listElem.innerText = num;
- });
+           listElem.addEventListener('click', strongCounter);
         }
         list.appendChild(listElem)
     }
